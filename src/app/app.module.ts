1670234704f886b93d663578264da7ae4aebf068
charlotteasencio <charlotte.e.asencio@gmail.com>
2019-01-01 17:14:29 -0500

    inital commit of homepage and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+import { MatIconModule} from '@angular/material/icon';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  openFullMenu() {
+    document.getElementById('menuButton').classList.toggle("hidden");
+    document.getElementById('fullMenu').classList.toggle("hidden");
+    //document.getElementById('closeButton').classList.toggle('hidden');
+  }
+}
